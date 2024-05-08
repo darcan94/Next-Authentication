070ddb76ab0b8d6cbe67bce371b9bd3e3b886222
darcan94 <darcanf.94@gmail.com>
2024-05-08 15:49:21 -0400

    Improve login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,9 @@
-import Link from "next/link";
 import { LoginForm } from "../ui/login-form";
 
 export default function LoginPage(){
     return (
-        <main className="h-screen flex items-center justify-center">
-            <div className="w-3/12 h-auto flex flex-col gap-4 justify-center items-center p-4 bg-zinc-300 rounded-lg">
-                <h1>Login</h1>
-                <LoginForm />
-            </div>
-            <Link href='/'>Back</Link>
+        <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-[#0c1023] to-[#1e2949] px-4 py-12 sm:px-6 lg:px-8">
+            <LoginForm />
         </main>
-
     )
 }
--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -1,37 +1,90 @@
 'use client'
 import {login} from '@/app/actions/auth'
+import Link from 'next/link'
 import {useFormState, useFormStatus} from 'react-dom'
 
 export function LoginForm() {
     const [state, action] = useFormState(login, undefined)
     return (
-        <form className="w-full flex flex-col gap-4" action={ action }>
+        <div className='w-full max-w-md space-y-8'>
             <div>
-                <label className="block" htmlFor="email">Email</label>
-                <input className="w-full p-2 rounded-md" id="email" name="email" type="email" placeholder="Email" />
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">Sign in to your account</h2>
+                <p className="mt-2 text-center text-sm text-gray-400">
+                    <span>Or </span>
+                    <Link className="font-medium text-indigo-500 hover:text-indigo-400" href="/signup">
+                        sign up
+                    </Link>
+                </p>
             </div>
-            {state?.errors?.email && <p className="text-red-600">{state.errors.email}</p>}
+            <form className="space-y-6" action={ action }>
+                <div className='space-y-1'>
+                    <label className="block text-sm font-medium text-gray-400" htmlFor="email">Email address</label>
+                    <input 
+                        className="block w-full appearance-none rounded-md border 
+                                   border-gray-500 bg-gray-700 px-3 py-2 text-white 
+                                   placeholder-gray-400 focus:border-indigo-500 
+                                   focus:outline-none focus:ring-indigo-500 sm:text-sm" 
+                        autoComplete="email"
+                        id="email" 
+                        name="email" 
+                        type="email" 
+                        placeholder="Email" 
+                    />
+                </div>
+                {state?.errors?.email && <p className="text-red-600">{state.errors.email}</p>}
 
-            <div>
-                <label className="block" htmlFor="password">Password</label>
-                <input className="w-full p-2 rounded-md" id="password" name="password" type="password" />
-            </div>
-            <div>
-                {
-                    state?.errors?.password && (
-                        <div className="text-red-600">
-                            <p>Password must be:</p>
-                            <ul>
-                                {state.errors.password.map( error => (
-                                    <li key={error}> - {error}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    )
-                }
-            </div>
-            <LoginButton />
-        </form>
+                <div className='space-y-1'>
+                    <label className="block text-sm font-medium text-gray-400" htmlFor="password">Password</label>
+                    <input 
+                        className="block w-full appearance-none rounded-md border 
+                                   border-gray-500 bg-gray-700 px-3 py-2 text-white 
+                                   placeholder-gray-400 focus:border-indigo-500 
+                                   focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        id="password" 
+                        name="password" 
+                        placeholder="Password"
+                        type="password" 
+                    />
+
+                    <div className="ml-3">
+                        <button className="text-gray-400 hover:text-gray-300">
+                            <EyeIcon className="h-5 w-5" />
+                            <span className="sr-only">Show password</span>
+                        </button>
+                    </div>
+                </div>
+                    {
+                        state?.errors?.password && (
+                            <div className="text-red-600">
+                                <p>Password must be:</p>
+                                <ul>
+                                    {state.errors.password.map( error => (
+                                        <li key={error}> - {error}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )
+                    }
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                        <input type='checkbox'
+                            className="h-4 w-4 rounded text-indigo-600 focus:ring-indigo-500"
+                            id="remember-me"
+                            name="remember-me"/>
+
+                        <label className="ml-2 block text-sm text-gray-400" htmlFor="remember-me">
+                            Remember me
+                        </label>
+                    </div>
+                    <div className="text-sm">
+                        <Link className="font-medium text-indigo-500 hover:text-indigo-400" href="#">
+                            Forgot your password?
+                        </Link>
+                    </div>
+                </div>
+                <LoginButton />
+            </form>
+        </div>
     )
 }
 
@@ -40,8 +93,19 @@ function LoginButton() {
     const { pending } = useFormStatus()
 
     return (
-        <button className="bg-blue-500 p-4 w-full rounded-md text-white" aria-disabled={pending} type="submit">
-            {pending ? 'Submitting...' : 'Login'}
+        <button  
+            className="flex w-full justify-center rounded-md border border-transparent 
+                       bg-indigo-600 py-3 px-4 text-sm font-medium text-white shadow-sm 
+                       hover:bg-indigo-700 focus:outline-none focus:ring-2 
+                       focus:ring-indigo-500 focus:ring-offset-2"
+            aria-disabled={pending} type="submit">
+            {pending ? 'Submitting...' : 'Sign In'}
         </button>
+    )
+}
+
+function EyeIcon({ className}: {className: string}){
+    return(
+        <></>
     )
 }
