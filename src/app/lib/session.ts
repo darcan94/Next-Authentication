8836dcaa543b0d9e5f62250ebdcbe168a4e85e45
darcan94 <darcanf.94@gmail.com>
2024-05-13 09:55:21 -0400

    Change env variable name

--- src/app/lib/session.ts
+++ src/app/lib/session.ts
@@ -2,7 +2,7 @@ import 'server-only'
 import { SignJWT, jwtVerify } from 'jose'
 import {cookies} from "next/headers";
 
-const secretKey = process.env.SESSION_SECRET
+const secretKey = process.env.AUTH_SECRET
 const encodedKey = new TextEncoder().encode(secretKey)
 
 export async function encrypt(payload: any) {
