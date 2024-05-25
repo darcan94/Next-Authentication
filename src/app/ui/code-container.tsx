d1b7440f573c45fd0c7fc398f1bba956ac0e16ac
Dario Candia <darcanf.94@gmail.com>
2024-05-24 22:57:10 -0400

    Add Tabs from next ui

--- src/app/ui/code-container.tsx
+++ src/app/ui/code-container.tsx
@@ -1,32 +1,30 @@
+'use client'
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight"
 import { sourceCodes } from "./code"
-import { Fragment } from "react"
 import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs"
+import {Tabs, Tab} from "@nextui-org/tabs";
 
 export default function CodeContainer(){
     return (
-        <div className="flex flex-col gap-2 w-8/12 h-5/6 bg-gray-950 rounded-md p-4 shadow-lg">
-          <div className="flex items-center space-x-2">
-            <div className="h-3 w-3 rounded-full bg-red-500" />
-            <div className="h-3 w-3 rounded-full bg-yellow-500" />
-            <div className="h-3 w-3 rounded-full bg-green-500" />
-          </div>
-          <div role="tablist" className={`tabs tabs-xs w-full grid-cols-${sourceCodes.length} text-wrap overflow-y-auto overflow-x-hidden`}>
-            {
-              sourceCodes.map( (sourceCode, index) => (
-                <Fragment key={index}>
-                    <input type="radio" name="my_tabs_2" role="tab" className="tab sticky top-0 bg-gray-950 focus:text-cyan-400 text-left" aria-label={sourceCode.fileName}/>
-                    <pre role="tabpanel" className="tab-content">
-                      <SyntaxHighlighter 
-                          language={sourceCode.lang}
-                          style={atomOneDark}
-                          customStyle={{margin: 0, background: "transparent"}}>
-                            {sourceCode.code}
-                      </SyntaxHighlighter>
-                    </pre>
-                </Fragment>))
-            }
-          </div>
+        <div className="flex flex-col gap-2 w-10/12 h-5/6 bg-gray-950 rounded-md p-4 shadow-lg">
+
+          <Tabs items={sourceCodes}>
+              {sourceCodes.map(( sourceCode, index ) => (
+                  <Tab key={index} title={sourceCode.fileName}>
+                      <SyntaxHighlighter
+                         language={sourceCode.lang}
+                         style={atomOneDark}
+                         customStyle={{margin: 0, background: "transparent"}}
+                         codeTagProps={{
+                             style: {
+                                 fontSize: ".8rem",
+                             },
+                         }}>
+                              {sourceCode.code}
+                      </SyntaxHighlighter>)
+                  </Tab>
+               ))}
+          </Tabs>
         </div>
     )
 }
